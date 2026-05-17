04bcdedabbc969468f142bb4c4ef679e9da3e4e0
tkeynertorres2 <tkeynertorres2@gmail.com>
2026-05-16 20:59:13 -0500

    Delete Python UNI.py/Logica y representacion.py

--- Python UNI.py/Logica y representacion.py
+++
@@ -1,6 +0,0 @@
-n1=float(input("Ingrese La primera nota:"))
-n2=float(input("Ingrese La segunda nota:"))
-n3=float(input("Ingrese La tercera nota:"))
-suma=n1+n2+n3
-definitiva=suma/3
-mensaje=f"Bien su defintiva es {definitiva}" if definitiva>=3 else f"Sobresliente, su defintiva es: {definitiva}" if definitiva >=4 else f"Excelente, su definitiva es {definitiva}" if definitiva==5 else f"Insuficiente su definitva es: {definitiva}" if definitiva<3
